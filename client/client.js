4a75a4c49f0d82d9dcdf759f1a7d5c61ae78dcac
Lauricio Su <me@lauricio.com>
2013-12-23 19:26:27 +0200

    first implementation

--- client/client.js
+++ client/client.js
@@ -1,6 +1,15 @@
+sm = {};
+
 Template.hello.created = function () {
   Meteor.defer(function () {
     console.log('created')
+
+    var leftMenuElList = document.getElementById('menu-left-list');
+    var list = new ionic.views.ListView({
+      el: leftMenuElList
+    });
+
+
 
     var contentEl = document.getElementById('content');
     var content = new ionic.views.SideMenuContent({
@@ -19,11 +28,36 @@ Template.hello.created = function () {
       width: 270
     });
 
-    var sm = new ionic.controllers.SideMenuController({
+    sm = new ionic.controllers.SideMenuController({
       content: content,
       left: leftMenu,
       right: rightMenu
     });
 
+    $('body').addClass("platform-ios7");
 
   })}
+
+
+  Template.hello.events({
+    'click #toggle-left-menu' :function () {
+      sm.toggleLeft();
+  },
+      'click #toggle-right-menu' :function () {
+      sm.toggleRight();
+  },
+      'click #open-modal' :function () {
+        $( "#mainModal" ).addClass( "is-visible" );
+        StatusBar.hide();
+    },
+      'click #close-modal' :function () {
+        $( "#mainModal" ).removeClass( "is-visible" );
+        StatusBar.show();
+    }
+
+
+  // ,
+  //   'click .list' :function () {
+  //     sm.close();
+  // }
+  });
